9939924df478f3847ce2b827b3a12c3a6051f9fe
Pavlo <ravliv.pavlobohdan@gmail.com>
2021-12-11 16:21:01 +0100

    feat: hamburger menu with and responsive

--- src/components/layouts/LayoutComponent.tsx
+++ src/components/layouts/LayoutComponent.tsx
@@ -13,23 +13,23 @@ const Layout: FunctionComponent = ({ children }) => {
   return (
     <div>
       <Loading isRouteChanging={state.isRouteChanging} key={state.loadingKey} />
-      <div className="h-screen flex justify-center py-16 bg-purple-500">
-        <div className="mx-16 bg-purple-400 w-full h-full rounded-3xl shadow-sm relative">
-          <nav className="flex w-full justify-between align-baseline items-end pt-7">
-            <span className="text-2xl sm:text-5xl font-bold pl-24 text-white text-shadow-lg">
-              <Link href="/main">RNArefiner</Link>
-            </span>
-
-            <button
-              type="button"
-              className="flex items-center space-x-2 focus:outline-none "
-              onClick={(): void => setIsHamburgerOpen((prevState) => !prevState)}
-            >
-              <span>{isHamburgerOpen ? 'Open' : 'Close'}</span>
-              <HamburgerMenu isOpen={isHamburgerOpen} />
-            </button>
-
-            <div className="hidden md:flex justify-around w-1/4 ">
+      <div className="flex justify-center md:py-16 bg-purple-500 w-full">
+        <div className="md:mx-16 bg-purple-400 w-full h-full rounded-3xl shadow-sm md:relative">
+          <nav className="flex flex-col md:flex-row w-full justify-between align-baseline items-end pt-7">
+            <div className="flex gap-4">
+              <span className="text-2xl sm:text-5xl font-bold pl-24 text-white text-shadow-lg">
+                <Link href="/main">RNArefiner</Link>
+              </span>
+              <button
+                type="button"
+                className="flex items-center justify-between focus:outline-none md:hidden"
+                onClick={(): void => setIsHamburgerOpen((prevState) => !prevState)}
+              >
+                <span>{isHamburgerOpen ? 'Open' : 'Close'}</span>
+                <HamburgerMenu isOpen={isHamburgerOpen} />
+              </button>
+            </div>
+            <div className={`${isHamburgerOpen ? 'hidden' : ''} w-full text-center md:contents`}>
               <NavigationLink href="/about" />
               <NavigationLink href="/help" />
               <NavigationLink href="/contact" />
