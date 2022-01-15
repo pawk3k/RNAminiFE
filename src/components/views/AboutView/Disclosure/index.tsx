dd2134ce67dd9f6ae354e3939441fab3caf84be7
Pavlo <ravliv.pavlobohdan@gmail.com>
2022-01-15 12:04:12 +0100

    style: add some description to summary page

--- src/components/views/AboutView/Disclosure/index.tsx
+++ src/components/views/AboutView/Disclosure/index.tsx
@@ -5,10 +5,10 @@ const Disclosure: FunctionComponent<{ summary: string; details: string }> = ({
   details,
 }) => (
   <details className="mx-24 text-1xl text-dashas-purple bg-pink-100 mb-10 rounded-xl shadow ">
-    <summary className="font-bold text-xl cursor-pointer py-1 pl-5">
+    <summary className="font-bold text-xl cursor-pointer py-1 px-10">
       <span className="">{summary}</span>
     </summary>
-    <div className="ml-9">{details}</div>
+    <div className="ml-9 whitespace-pre">{details}</div>
   </details>
 );
 
--- src/components/views/AboutView/index.tsx
+++ src/components/views/AboutView/index.tsx
@@ -4,8 +4,11 @@ import Disclosure from './Disclosure';
 const AboutView: FunctionComponent = () => (
   <div className="mt-20">
     <Disclosure summary="About" details="RNArefiner is a bio informatics tool" />
-    <Disclosure summary="Authors" details="kek" />
-    <Disclosure summary="Acknowledgements and Funding" details="kek" />
+    <Disclosure
+      summary="Authors"
+      details={['Pavlo Ravliv\n', 'Denys Hromniuk\n', 'Daria Poda\n', 'Roman Pylnyk\n'].join('')}
+    />
+    <Disclosure summary="Acknowledgements and Funding" details="None" />
   </div>
 );
 
