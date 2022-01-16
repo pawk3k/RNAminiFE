2db891b2d4e640261014be2ee0db07b5b1796dfe
Pavlo <ravliv.pavlobohdan@gmail.com>
2022-01-16 13:01:02 +0100

    style: add proper logs remove submision time

--- src/components/views/OutputView/index.tsx
+++ src/components/views/OutputView/index.tsx
@@ -12,7 +12,7 @@ const Molstar = dynamic(() => import('./Molstar'), {
 
 const OutputView: FunctionComponent = () => {
   const { query } = useRouter();
-  const { data: { status, data, solution, superComposition, filteredPdb } = {}, isError } =
+  const { data: { status, data,logs, solution, superComposition, filteredPdb } = {}, isError } =
     useGetStatus();
 
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
@@ -23,12 +23,11 @@ const OutputView: FunctionComponent = () => {
         <div className="w-2/3 my-auto h-full flex justify-around items-center text-dashas-purple">
           <div className="w-full mx-9 px-7 relative bg-dashas-pink rounded-3xl h-60 flex justify-start items-start flex-col">
             <div className="mt-11">Task : {query.uid}</div>
-            <div>Submission time: time</div>
             <div className="flex justify-center w-full items-center mt-3 text-3xl ">
               Your solution is ready!
             </div>
             <DownloadTaskButton file={data ?? ''} text="Download task" />
-            <DownloadTaskButton file={data ?? ''} text="Logs" />
+            <DownloadTaskButton file={logs ?? ''} text="Logs" />
             <DownloadTaskButton file={solution ?? ''} text="Download solution" />
           </div>
         </div>
@@ -41,7 +40,6 @@ const OutputView: FunctionComponent = () => {
     <div className="my-auto w-full h-full flex justify-center items-center text-dashas-purple">
       <div className="pl-9 relative bg-dashas-pink rounded-3xl w-2/3 h-60 flex justify-start items-start flex-col">
         <div className="mt-11">Task : {query.uid}</div>
-        <div>Submission time: time</div>
         <div
           className={`self-center mt-2 text-2xl whitespace-pre-wrap text-center ${
             isError && 'text-dashas-red'
