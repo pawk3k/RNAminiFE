b7cd8c632f7689cd6dcc1b711cf8303c16d7079f
Pavlo <ravliv.pavlobohdan@gmail.com>
2022-01-16 20:09:09 +0100

    refactor: remove input from molstar

--- src/components/views/OutputView/Molstar/index.tsx
+++ src/components/views/OutputView/Molstar/index.tsx
@@ -1,11 +1,4 @@
-import {
-  ChangeEvent,
-  FunctionComponent,
-  useEffect,
-  useLayoutEffect,
-  useRef,
-  useState,
-} from 'react';
+import { FunctionComponent, useEffect, useLayoutEffect, useRef, useState } from 'react';
 import { ColorNames } from 'molstar/lib/mol-util/color/names';
 import { MolstarWrapper } from './MolstarWrapper';
 
@@ -15,29 +8,6 @@ const Molstar: FunctionComponent<{ inputFile1: string; inputFile2: string }> = (
 }) => {
   const parent = useRef<HTMLDivElement>(null);
   const [molstarPlugin, setMolstarPlugin] = useState<MolstarWrapper | undefined>();
-
-  const [file1, setFile1] = useState<string>();
-  const [file2, setFile2] = useState<string>();
-
-  const handleReadAllFiles = async (e: ChangeEvent<HTMLInputElement>): Promise<void> => {
-    e.preventDefault();
-    const { files } = e.target;
-    // eslint-disable-next-line @typescript-eslint/no-unused-expressions
-    files &&
-      Object.keys(files as FileList).forEach((i: string) => {
-        const file = files[+i];
-        const reader = new FileReader();
-        reader.onload = (event): void => {
-          const text = event.target?.result as string;
-          if (i === '0') {
-            setFile1(text);
-          } else {
-            setFile2(text);
-          }
-        };
-        reader.readAsText(file);
-      });
-  };
 
   useLayoutEffect(() => {
     if (parent.current) {
@@ -57,14 +27,13 @@ const Molstar: FunctionComponent<{ inputFile1: string; inputFile2: string }> = (
       }
     }
     initViewer3d();
-  }, [file1, file2, inputFile1, inputFile2, molstarPlugin]);
+  }, [inputFile1, inputFile2, molstarPlugin]);
 
   return (
     <div>
       <div id="viewer3d-molstar" className="w-96 h-96 -ml-3">
         <div id="structure-3d-viewer" ref={parent} />
       </div>
-      {/* <input type="file" onChange={handleReadAllFiles} multiple /> */}
     </div>
   );
 };
