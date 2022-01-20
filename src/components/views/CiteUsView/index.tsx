727cebba9353745b38a5dee4fad89a3bf65d58c2
Pavlo <ravliv.pavlobohdan@gmail.com>
2022-01-20 14:35:34 +0100

    style: update cite us page

--- src/components/views/CiteUsView/index.tsx
+++ src/components/views/CiteUsView/index.tsx
@@ -1,13 +1,19 @@
 import { FunctionComponent } from 'react';
 
 const CiteUsView: FunctionComponent = () => (
-  <div>
-    Citations Any published work which has made use of RNArefiner should cite the following paper:
-    M. Antczak, D. Poda, R. Pylnyk, P. Ravliv, D. Hromniuk, M. Popenda, M. Szachniuk, RNArefiner: a
-    webserver for stereochemical refinement of RNA 3D structures The following paper can be also
-    cited: Popenda M, Zok T, Sarzynska J, Korpeta A, Adamiak RW, Antczak M, Szachniuk M (2021)
-    Entanglements of structure elements revealed in RNA 3D models,Nucleic Acids Research, 2021,
-    49(17):9625-9632 (doi:10.1093/nar/gkab716).
+  <div className="px-14 flex flex-col gap-5">
+    <p>Citations</p>
+    <p>Any published work which has made use of RNArefiner should cite the following paper:</p>
+    <p>
+      M. Antczak, D. Poda, R. Pylnyk, P. Ravliv, D. Hromniuk, M. Popenda, M. Szachniuk, RNArefiner:
+      a webserver for stereochemical refinement of RNA 3D structures
+    </p>
+    <p>The following paper can be also cited:</p>
+    <p>
+      Popenda M, Zok T, Sarzynska J, Korpeta A, Adamiak RW, Antczak M, Szachniuk M (2021)
+      Entanglements of structure elements revealed in RNA 3D models,Nucleic Acids Research, 2021,
+      49(17):9625-9632 (doi:10.1093/nar/gkab716).
+    </p>
   </div>
 );
 
