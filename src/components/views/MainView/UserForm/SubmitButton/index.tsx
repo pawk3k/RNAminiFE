14a00c9ef0636ed327c25b4be01ccc912ba98bfd
Pavlo <ravliv.pavlobohdan@gmail.com>
2022-01-20 19:10:46 +0100

    style: adjust loader on the button

--- src/components/views/MainView/UserForm/SubmitButton/index.tsx
+++ src/components/views/MainView/UserForm/SubmitButton/index.tsx
@@ -12,7 +12,8 @@ const SubmitButton: FunctionComponent<{ isDisabled: boolean; isLoading: boolean 
   const [file] = useFileContext();
   const [isRouteChanging, setRouteChanging] = useState(false);
   const router = useRouter();
-  const submitDisabled = !file && characters.join('').length < 4;
+  const submitDisabled =
+    (!file && characters.join('').length < 4) || isDisabled || isLoading || isRouteChanging;
 
   useEffect(() => {
     router.events.on('routeChangeStart', (): void => setRouteChanging(true));
@@ -27,7 +28,7 @@ const SubmitButton: FunctionComponent<{ isDisabled: boolean; isLoading: boolean 
   return (
     <div className="flex mb-2 w-full">
       <button
-        disabled={submitDisabled || isDisabled || isLoading}
+        disabled={submitDisabled}
         type="submit"
         className="disabled:bg-slate-400 disabled:shadow-none disabled:text-green-50 shadow-md text-dashas-purple shadow-dashas-purple mt-6 relative rounded-3xl mx-auto w-1/2 h-10 text-center duration-300 bg-dashas-pink  transform focus:translate-y-1"
       >
