437a997126d0a7dd127b696983586fdb36d49f73
Pavlo <ravliv.pavlobohdan@gmail.com>
2022-01-20 18:18:25 +0100

    chore: comment out mol probity

--- src/components/uiKit/Table/Table.types.ts
+++ src/components/uiKit/Table/Table.types.ts
@@ -1,9 +1,9 @@
-import { Column } from 'react-table';
+// import { Column } from 'react-table';
 
-export type { Column };
+// export type { Column };
 
-export interface TableProps<T extends object> {
-  columns: Column<T>[];
-  data: T[];
-  totalCount?: number;
-}
+// export interface TableProps<T extends object> {
+//   columns: Column<T>[];
+//   data: T[];
+//   totalCount?: number;
+// }
--- src/components/uiKit/Table/index.tsx
+++ src/components/uiKit/Table/index.tsx
@@ -1,40 +1,40 @@
-import { useTable } from 'react-table';
+// import { useTable } from 'react-table';
 
-import { TableProps } from './Table.types';
+// import { TableProps } from './Table.types';
 
-const Table = <T extends object = object>({ data, columns }: TableProps<T>): JSX.Element => {
-  const { getTableProps, getTableBodyProps, headerGroups, rows, prepareRow } = useTable<T>({
-    columns,
-    data,
-  });
+// const Table = <T extends object = object>({ data, columns }: TableProps<T>): JSX.Element => {
+//   const { getTableProps, getTableBodyProps, headerGroups, rows, prepareRow } = useTable<T>({
+//     columns,
+//     data,
+//   });
 
-  return (
-    <table {...getTableProps()}>
-      <thead>
-        {headerGroups.map((headerGroup) => (
-          <tr {...headerGroup.getHeaderGroupProps()}>
-            {headerGroup.headers.map((column) => (
-              <th {...column.getHeaderProps()}>{column.render('Header')}</th>
-            ))}
-          </tr>
-        ))}
-      </thead>
-      <tbody {...getTableBodyProps()}>
-        {rows.map((row) => {
-          prepareRow(row);
-          return (
-            <tr {...row.getRowProps()}>
-              {row.cells.map((cell) => (
-                <td {...cell.getCellProps()}>{cell.render('Cell')}</td>
-              ))}
-            </tr>
-          );
-        })}
-        <tr>
-          <td className="w-full text-center" colSpan={columns.length} />
-        </tr>
-      </tbody>
-    </table>
-  );
-};
-export default Table;
+//   return (
+//     <table {...getTableProps()}>
+//       <thead>
+//         {headerGroups.map((headerGroup) => (
+//           <tr {...headerGroup.getHeaderGroupProps()}>
+//             {headerGroup.headers.map((column) => (
+//               <th {...column.getHeaderProps()}>{column.render('Header')}</th>
+//             ))}
+//           </tr>
+//         ))}
+//       </thead>
+//       <tbody {...getTableBodyProps()}>
+//         {rows.map((row) => {
+//           prepareRow(row);
+//           return (
+//             <tr {...row.getRowProps()}>
+//               {row.cells.map((cell) => (
+//                 <td {...cell.getCellProps()}>{cell.render('Cell')}</td>
+//               ))}
+//             </tr>
+//           );
+//         })}
+//         <tr>
+//           <td className="w-full text-center" colSpan={columns.length} />
+//         </tr>
+//       </tbody>
+//     </table>
+//   );
+// };
+// export default Table;
