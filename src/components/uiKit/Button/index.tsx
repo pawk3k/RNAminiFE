1494815874eff8c8908a80d60c0839dd2e2bb60e
Pavlo <ravliv.pavlobohdan@gmail.com>
2022-02-02 18:53:41 +0100

    chore: add styled-components

--- src/components/uiKit/Button/index.tsx
+++ src/components/uiKit/Button/index.tsx
@@ -1,11 +1,8 @@
-import { ComponentPropsWithoutRef, FunctionComponent } from 'react';
+import styled from 'styled-components';
 
-const Button: FunctionComponent<ComponentPropsWithoutRef<'button'>> = ({ ...props }) => (
-  <button
-    {...props}
-    type="submit"
-    className="relative mx-auto mt-6 h-10 w-1/2 transform rounded-3xl bg-dashas-pink text-center text-dashas-purple shadow-md shadow-dashas-purple duration-300 focus:translate-y-1 disabled:bg-slate-400  disabled:text-green-50 disabled:shadow-none"
-  />
-);
+const Button = styled.button.attrs({
+  className:
+    'relative mx-auto mt-6 h-10 w-1/2 transform rounded-3xl bg-dashas-pink text-center text-dashas-purple shadow-md shadow-dashas-purple duration-300 focus:translate-y-1 disabled:bg-slate-400  disabled:text-green-50 disabled:shadow-none',
+})``;
 
 export default Button;
---
+++ src/components/uiKit/Typography/index.tsx
@@ -0,0 +1,60 @@
+import styled from 'styled-components';
+
+export const Heading1 = styled.h1.attrs({ className: 'text-denim-blue font-extrabold' })`
+  font-size: 40px;
+  line-height: 54.56px;
+`;
+
+export const Heading2 = styled.h2.attrs({ className: 'text-denim-blue font-extrabold' })`
+  font-size: 32px;
+  line-height: 43.65px;
+`;
+
+export const Heading3 = styled.h3.attrs({
+  className: 'text-denim-blue font-bold',
+})`
+  font-size: 28px;
+  line-height: 38.19px;
+`;
+
+export const Heading4 = styled.h4.attrs({
+  className: 'text-denim-blue font-bold',
+})`
+  font-size: 24px;
+  line-height: 32.74px;
+`;
+
+export const Heading5 = styled.h5.attrs({
+  className: 'text-denim-blue font-semibold',
+})`
+  font-size: 22px;
+  line-height: 30.01px;
+`;
+
+export const Heading6 = styled.h6.attrs({
+  className: 'text-denim-blue font-semibold',
+})`
+  font-size: 18px;
+  line-height: 24.55px;
+`;
+
+export const Body = styled.p.attrs({
+  className: 'text-nevada font-normal',
+})`
+  font-size: 16px;
+  line-height: 21.82px;
+`;
+
+export const BodySmall = styled.p.attrs({
+  className: 'text-nevada font-normal',
+})`
+  font-size: 14px;
+  line-height: 19.1px;
+`;
+
+export const BodyUltraSmall = styled.p.attrs({
+  className: 'text-nevada font-normal',
+})`
+  font-size: 12px;
+  line-height: 16.37px;
+`;
